d428ad36c94fead079a9f15f8c44e5cdb9b04e9e
Onur <onurozet@hotmail.com>
2024-05-06 08:49:29 +0300

    add loading effect using nextjs

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className="flex justify-center mt-16">
+            <img className="h-52" src="spinner.svg" alt="loading..."/>
+        </div>
+    );
+};
+
+export default Loading;
