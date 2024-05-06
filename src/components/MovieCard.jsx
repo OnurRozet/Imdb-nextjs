653582f803ca7cf478258685854c4f87217856d4
Onur <onurozet@hotmail.com>
2024-05-06 10:16:40 +0300

    MovieCard added and page design finished

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import {FiThumbsUp} from "react-icons/fi";
+
+const MovieCard = ({data}) => {
+    return (
+        <div
+            className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
+            <Link href={`/movie/${data.id}`}>
+                <Image src={`https://image.tmdb.org/t/p/original/${data.backrop_path || data.poster_path}`} width={500}
+                       height={300}
+                       alt={"image movie"}
+                       className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300"/>
+                <div className="p-2 flex items-center justify-center flex-col">
+                    <p className="line-clamp-2 text-md text-center">{data.overview}</p>
+                    <h2 className="text-lg font-bold truncate">{data.title || data.original_name}</h2>
+                    <p className="flex items-center justify-center gap-2">
+                        {data.release_date || data.first_air_date} - {data.vote_average} <FiThumbsUp/>
+                    </p>
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export default MovieCard;
--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import MovieCard from "@/components/MovieCard";
 
 const MovieItem = ({data}) => {
     return (
-        <div>
+        <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto p-4">
             {
                 data.map((item, index) => {
                     return (
                         <div key={index}>
-                            {/*<img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.title}/>*/}
-                            <h3>{item.title}</h3>
+                           <MovieCard data={item} />
                         </div>
                     );
                 })
