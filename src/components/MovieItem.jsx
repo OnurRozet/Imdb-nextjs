6580a7bac7013152453d024f3e6fad657361f67a
Onur <onurozet@hotmail.com>
2024-05-08 11:41:19 +0300

    console.log removed

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import MovieCard from "@/components/MovieCard";
 
 const MovieItem = ({data}) => {
-    console.log("data",data);
     return (
         <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-full mx-auto p-4">
             {
